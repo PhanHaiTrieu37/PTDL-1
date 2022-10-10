486e373d4acec86f7c35d74079a9ec6977125c70
Phan Hai Trieu <trieuphan3701@gmail.com>
2022-10-11 00:57:21 +0700

    day10/10 1st

---
+++ pandas/reduce-usage.py
@@ -0,0 +1,30 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "Python 3.8.5 32-bit",
+   "language": "python",
+   "name": "python3"
+  },
+  "language_info": {
+   "name": "python",
+   "version": "3.8.5"
+  },
+  "orig_nbformat": 4,
+  "vscode": {
+   "interpreter": {
+    "hash": "f594cce056f2b70017d1ca589b2a8f3cd84c9e6b4ffb58b7eca5f8b6d97b73b7"
+   }
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 2
+}
